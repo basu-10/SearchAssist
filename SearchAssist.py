21b373f81797a5adf69853d4f8a99ec2dfd438fd
Asesh Basu <105117710+basu-10@users.noreply.github.com>
2023-07-26 13:49:36 +0530

    Update SearchAssist.py
    saves api,cse key on first run. fetches values from saved loacation for subsequent usage

--- SearchAssist.py
+++ SearchAssist.py
@@ -1,46 +1,50 @@
 '''
 can run as:
-python SearchAssist.py
+python SearchAssist.py --q "SEARCH QUERY"
 OR,
-python SearchAssist.py --api-key YOUR_API_KEY --cse-key YOUR_CSE_KEY --query SEARCH_QUERY
+python SearchAssist.py --api-key YOUR_API_KEY --cse-key YOUR_CSE_KEY --q "SEARCH QUERY"
 
 provide your own YOUR_API_KEY, YOUR_CSE_KEY, SEARCH_QUERY
 '''
+
 from googleapiclient.discovery import build
 import pprint
-import argparse
 import logging
+
+import argparse
+import os
 from dotenv import dotenv_values
 
 # Set up logging configuration
-logging.basicConfig(filename='error.log', level=logging.ERROR)
+logging.basicConfig(filename="error.log", level=logging.ERROR)
 
 config = dotenv_values(".env.secret")
+
 
 class GoogleSearchAPI:
     def __init__(self, api_key, cse_key):
         self.api_key = api_key
         self.cse_key = cse_key
-        self.resource = build("customsearch", 'v1', developerKey=self.api_key).cse()
+        self.resource = build("customsearch", "v1", developerKey=self.api_key).cse()
 
     def search(self, query):
         try:
             result_raw = self.resource.list(q=query, cx=self.cse_key).execute()
-            with open("result_raw.txt", 'w') as f:
+            with open("result_raw.txt", "w") as f:
                 f.write(str(result_raw))
             return result_raw
         except Exception as e:
-            logging.error(f'Error {e} occurred')
+            logging.error(f"Error {e} occurred")
 
     def trim_results(self, result_raw):
         result_trimmed = []
-        for i in result_raw['items']:
-            title = i['title']
-            snippet = i['snippet']
-            url = i['link']
-            d = {'title': title, 'snippet': snippet, 'url': url}
+        for i in result_raw["items"]:
+            title = i["title"]
+            snippet = i["snippet"]
+            url = i["link"]
+            d = {"title": title, "snippet": snippet, "url": url}
             result_trimmed.append(d)
-        with open("result_trimmed.txt", 'w') as f:
+        with open("result_trimmed.txt", "w") as f:
             f.write(str(result_trimmed))
         return result_trimmed
 
@@ -49,21 +53,54 @@ class GoogleSearchAPI:
             pprint.pprint(i)
 
 
+class Process:
+    def save_to_env_file(api_key, cse_key, file_path):
+        with open(file_path, "w") as file:
+            file.write(f"API_KEY={api_key}\n")
+            file.write(f"CSE_KEY={cse_key}\n")
+
+    def get_keys_from_env(file_path):
+        config = dotenv_values(file_path)
+        api_key = config.get("API_KEY")
+        cse_key = config.get("CSE_KEY")
+        return api_key, cse_key
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Google Search API Client")
 
     # command line arguments
     parser.add_argument("--api-key", help="Your API Key")
     parser.add_argument("--cse-key", help="Your CSE Key")
-    parser.add_argument("--query", help="Search query")
+    parser.add_argument("--q", required=True, help="Search query")
 
     args = parser.parse_args()
+    env_file_path = ".env.secret"
 
-    api_key = args.api_key or config['api_key']
-    cse_key = args.cse_key or config['cse_key']
-    query = args.query or input("Enter your search query: ")
+    api_key, cse_key = "", ""
 
+    # Check if the API key and CSE key are provided as command-line arguments
+    if args.api_key and args.cse_key:
+        # Save the provided keys to the .env.secret file
+        Process.save_to_env_file(args.api_key, args.cse_key, env_file_path)
+    else:
+        # Check if the .env.secret file exists and load keys from there
+        if os.path.exists(env_file_path):
+            api_key, cse_key = Process.get_keys_from_env(env_file_path)
+        else:
+            print(".env.secret file not exists")
+
+        if not api_key or not cse_key:
+            print("Error: API key or CSE key not found in .env.secret.")
+            # Prompt the user to enter the keys if not found in the file
+            api_key = input("Enter your API key: ")
+            cse_key = input("Enter your CSE key: ")
+            Process.save_to_env_file(api_key, cse_key, env_file_path)
+    
+    print("Searching for:", args.q)
+    # search logic
     search_api = GoogleSearchAPI(api_key, cse_key)
-    result_raw = search_api.search(query)
+    result_raw = search_api.search(args.q)
     result_trimmed = search_api.trim_results(result_raw)
     search_api.pprint_results(result_trimmed)
+
